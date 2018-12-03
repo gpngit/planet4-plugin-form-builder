870b95a1a23f11d75e692b923cbb4c0739c695ec
Mike Little <mike@zed1.com>
2018-12-03 18:52:49 +0000

    Add single form template. Create basic twig views to ender form and its fields.

---
+++ content/plugins/planet4-form-builder/templates/single-p4_form.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Part of the Planet4 Form Builder.
+ */
+
+namespace P4FB\Form_Builder\Templates;
+
+use Timber\Post;
+use Timber\Timber;
+
+$context                    = Timber::get_context();
+$context['post']            = new Post();
+$context['form_submit_url'] = admin_url( 'admin_post.php' );
+$context['action']          = 'p4fb_form_submit';
+
+// process the field options for easier rendering
+foreach ( $context['post']->p4_form_fields as $index => $field ) {
+	if ( ( 'select' === $field['type'] ) || ( 'checkbox-group' === $field['type'] ) || ( 'radio-group' === $field['type'] ) ) {
+		$options     = $field['options'];
+		$options     = explode( "\n", $options );
+		$new_options = [];
+		foreach ( $options as $option ) {
+			if ( empty( trim( $option ) ) ) {
+				continue;
+			}
+			$parts = explode( '|', $option, 2 );
+			if ( count( $parts ) > 1 ) {
+				$new_options[ trim( $parts[0] ) ] = trim( $parts[1] );
+			} else {
+				$new_options[ trim( $option ) ] = trim( $option );
+			}
+		}
+		$context['post']->p4_form_fields[ $index ]['options'] = $new_options;
+	}
+}
+
+Timber::render(
+	[
+		'single-' . $post->post_type . '.twig',
+		'single.twig',
+	],
+	$context
+);
