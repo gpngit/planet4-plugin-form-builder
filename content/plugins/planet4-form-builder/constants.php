bf59a06f07c13714770164a28e1bc620c0ba778b
Mike Little <mike@zed1.com>
2019-02-12 16:34:32 +0000

    Add defined prefix for use elsewhere.

--- content/plugins/planet4-form-builder/constants.php
+++ content/plugins/planet4-form-builder/constants.php
@@ -11,9 +11,19 @@ namespace P4FB\Form_Builder;
 define( 'P4FB_FORM_CPT', 'p4-form' );
 
 /**
+ * The prefix for form metakey names etc.
+ */
+define( 'P4FB_KEY_PREFIX', 'p4_form_' );
+
+/**
  * The internal Mapping CPT name.
  */
 define( 'P4FB_MAPPING_CPT', 'p4-mapping' );
+
+/**
+ * The prefix for mapping metakey names etc.
+ */
+define( 'P4FB_MAPPING_KEY_PREFIX', 'p4_form_mapping_' );
 
 /**
  * The internal Entry CPT name.
@@ -24,7 +34,6 @@ define( 'P4FB_ENTRY_CPT', 'p4-form-entry' );
  * The plugins root directory.
  */
 define( 'P4FB_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-
 
 /**
  * Option name for settings in options table
