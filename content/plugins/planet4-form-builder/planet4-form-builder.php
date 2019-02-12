5248d5905aa68e7dec8aeb06e4093a1494f54571
Mike Little <mike@zed1.com>
2019-02-12 16:42:10 +0000

    Add new classes. Instantiate them.

--- content/plugins/planet4-form-builder/planet4-form-builder.php
+++ content/plugins/planet4-form-builder/planet4-form-builder.php
@@ -14,21 +14,16 @@
 
 namespace P4FB\Form_Builder;
 
-/**
- * Followed WordPress plugins best practices from https://developer.wordpress.org/plugins/the-basics/best-practices/
- * Followed WordPress-Core coding standards https://make.wordpress.org/core/handbook/best-practices/coding-standards/php/
- * Followed WordPress-VIP coding standards https://vip.wordpress.com/documentation/code-review-what-we-look-for/
- * Added namespacing and PSR-4 auto-loading.
- */
-
 // Exit if accessed directly.
-defined( 'ABSPATH' ) or die( 'Direct access is forbidden !' );
+defined( 'ABSPATH' ) or die( 'Direct access is forbidden!' );
 
 require_once __DIR__ . '/constants.php';
 require_once __DIR__ . '/classes/class-form-builder.php';
 require_once __DIR__ . '/classes/class-form-mapping.php';
 require_once __DIR__ . '/classes/class-form-handler.php';
 require_once __DIR__ . '/classes/class-form-entry.php';
+require_once __DIR__ . '/classes/class-entry-handler.php';
+require_once __DIR__ . '/classes/class-entry-handler-bsd.php';
 if ( ! class_exists( 'Gamajo_Template_Loader' ) ) {
 	require plugin_dir_path( __FILE__ ) . 'class-gamajo-template-loader.php';
 }
@@ -38,9 +33,13 @@ add_action( 'plugins_loaded', function () {
 	Form_Mapping::get_instance()->load();
 	Form_Handler::get_instance()->load();
 	Form_Entry::get_instance()->load();
+	Entry_Handler::get_instance()->load();
+	Entry_Handler_BSD::get_instance()->load();
 } );
 
 if ( is_admin() ) {
 	require_once __DIR__ . '/classes/class-form-builder-settings-page.php';
+	require_once __DIR__ . '/classes/class-settings-page-bsd.php';
 	Form_Builder_Settings_Page::get_instance();
+	Settings_Page_BSD::get_instance();
 }
