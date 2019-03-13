ba67d291c91041a39d305ac702e27db74bd8669a
Mike Little <mike@zed1.com>
2019-03-13 18:16:22 +0000

    Transform to standalone plugin repo.

--- .config/local-config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Core settings.
-define( 'WP_DEFAULT_THEME', 'twentyseventeen' );
-
-// Set path to MU Plugins.
-defined( 'WP_CONTENT_URL' ) || define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
-defined( 'WPMU_PLUGIN_DIR' ) || define( 'WPMU_PLUGIN_DIR', '/chassis/content/plugins-mu' );
-defined( 'WPMU_PLUGIN_URL' ) || define( 'WPMU_PLUGIN_URL', WP_CONTENT_URL . '/plugins-mu' );
